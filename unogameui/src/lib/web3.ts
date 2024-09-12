4cee8ce7c51b9738b198075779e1b8db6031fdd9
Ayush <99096397+ayush4345@users.noreply.github.com>
2024-09-12 22:48:29 +0530

    integrated privy

--- unogameui/src/lib/web3.ts
+++ unogameui/src/lib/web3.ts
@@ -9,29 +9,29 @@ declare global {
 }
 
 async function verifyContract(provider: ethers.Provider, address: string) {
-    const code = await provider.getCode(address);
-    if (code === '0x') {
-      throw new Error('No contract deployed at the specified address');
-    }
-    console.log('Contract verified at address:', address);
+  const code = await provider.getCode(address);
+  if (code === '0x') {
+    throw new Error('No contract deployed at the specified address');
   }
+  console.log('Contract verified at address:', address);
+}
 
-export async function getContract() {
+export async function getContract(address: string) {
   if (typeof window !== 'undefined' && window.ethereum) {
     try {
       // await window.ethereum.request({ method: 'eth_requestAccounts' })
-      const provider = new ethers.BrowserProvider(window.ethereum)
-      
+      const provider = new ethers.JsonRpcProvider('https://sepolia-rpc.kakarot.org')
+
       // const network = await provider.getNetwork()
       // console.log('Connected to network:', network.name, 'Chain ID:', network.chainId)
 
-      const signer = await provider.getSigner()
+      const signer = await provider.getSigner(address)
       // console.log(signer)
       // const address = await signer.getAddress()
       // console.log(address)
 
       //TestNet
-      const contractAddress = '0x7e25d8b74cc92E114C9275D04C814c6Fef3E4036' 
+      const contractAddress = '0x7e25d8b74cc92E114C9275D04C814c6Fef3E4036'
       //Local
       //const contractAddress = '0x5FbDB2315678afecb367f032d93F642f64180aa3' 
       if (!contractAddress) {
@@ -39,7 +39,7 @@ export async function getContract() {
       }
       const contractABI = UNOContractJson.abi
 
-      
+
       await verifyContract(provider, contractAddress);
       const gameContract = new ethers.Contract(
         contractAddress!,
