b213fe8a60d56de25ee222f67cf12546b56b5cef
Ayush <99096397+ayush4345@users.noreply.github.com>
2024-10-02 23:13:14 +0530

    added spline animation

--- unogameui/src/components/homePage/Hero.jsx
+++ unogameui/src/components/homePage/Hero.jsx
@@ -4,8 +4,8 @@ import Spline from '@splinetool/react-spline';
 
 const Hero = () => (
   <section className={`sm:pb-16 xs:pb-8 pb-12 text-7xl text-center text-white font-bold`}>
-    zkUNO
-    {/* <Spline scene="https://prod.spline.design/1yrB6W5RKD2Ha5KE/scene.splinecode" /> */}
+    {/* zkUNO */}
+    <Spline scene="https://prod.spline.design/PP5UHnFUA0YPE-z0/scene.splinecode" />
   </section>
 );
 
