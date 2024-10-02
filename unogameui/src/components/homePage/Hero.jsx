180284d7c702e0762d297ab1d8aae4d36f02ee7d
Ayush <99096397+ayush4345@users.noreply.github.com>
2024-10-02 22:57:55 +0530

    removed spline

--- unogameui/src/components/homePage/Hero.jsx
+++ unogameui/src/components/homePage/Hero.jsx
@@ -4,7 +4,8 @@ import Spline from '@splinetool/react-spline';
 
 const Hero = () => (
   <section className={`sm:pb-16 xs:pb-8 pb-12 text-7xl text-center text-white font-bold`}>
-    <Spline scene="https://prod.spline.design/1yrB6W5RKD2Ha5KE/scene.splinecode" />
+    zkUNO
+    {/* <Spline scene="https://prod.spline.design/1yrB6W5RKD2Ha5KE/scene.splinecode" /> */}
   </section>
 );
 
